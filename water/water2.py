c1ad8091fcb060674a897c34109df838e460e7be
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2024-04-26 09:56:57 +0200

    print number local aware (comma)

--- water/water2.py
+++ water/water2.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
+import sys
+import locale
 
 import influxdb_client
+
+locale.setlocale(locale.LC_ALL, '')
 
 GPIO.setmode(GPIO.BCM)
 
@@ -29,8 +33,10 @@ client = influxdb_client.InfluxDBClient(
 )
 
 write_api = client.write_api(write_options=influxdb_client.client.write_api.SYNCHRONOUS)
-
 ctx=SignalContext(write_api)
+
+def ns_to_s(ns : int):
+    return ns / 1000000000
 
 def onSignal(null):
     global ctx
@@ -40,24 +46,37 @@ def onSignal(null):
         ctx.write_idx = 1 - ctx.write_idx
         ctx.timespan[ctx.write_idx] = curr_ns - ctx.last_ns
 
-        if ctx.timespan[0] != None and ctx.timespan[1] != None:
-            one_rotation_ns = ctx.timespan[0] + ctx.timespan[1]
-            liter_perhour = int ( ns_per_hour_times_two_signals / one_rotation_ns )
-            diff_s = one_rotation_ns / 1000000000
-            print(f'seconds/{liter_per_two_signals}l: {diff_s:8.3f}\t{liter_perhour:8} l/h (last diff: {ctx.timespan[ctx.write_idx]}) - {datetime.datetime.now().strftime("%Y.%m.%d, %H:%M:%S") }')
-            p = influxdb_client.Point("water").field("literperhour", liter_perhour)
-            ctx.write_api.write(bucket='wp', org='orgbee', record=p)
+    if ctx.timespan[0] != None and ctx.timespan[1] != None:
+        one_rotation_ns = ctx.timespan[0] + ctx.timespan[1]
+        liter_perhour = int ( ns_per_hour_times_two_signals / one_rotation_ns )
+
+        print(f'{liter_perhour:>8} l/h '
+              f'\t{(locale.format_string("%.2f", ns_to_s(one_rotation_ns), True)):>8} s/{liter_per_two_signals}l:' 
+              f'\t{(locale.format_string("%.2f", ns_to_s(ctx.timespan[ctx.write_idx]))):>8} s last signal '
+              f'\t{datetime.datetime.now().strftime("%Y.%m.%d %H:%M:%S")}')
+
+        p = influxdb_client.Point("water").field("literperhour", liter_perhour)
+        ctx.write_api.write(bucket='wp', org='orgbee', record=p)
 
     ctx.last_ns = curr_ns
 
-
-GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING, callback=onSignal, bouncetime=50)
-
-try:
+def fakeloop():
     while True:
-        time.sleep(600)
+        onSignal(23)
+        time.sleep(1)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    write_api.close()
-    client.close()
+def mainloop():
+    try:
+        while True:
+            time.sleep(600)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        write_api.close()
+        client.close()
+
+
+if 'fake' in sys.argv:
+    fakeloop()
+else:
+    GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING, callback=onSignal, bouncetime=50)
+    mainloop()
